43f4e3dfe851f6538019c18b2f3403cf65b2ae3c
szuwgh <895340293@qq.com>
2022-08-03 19:35:20 +0800

    add module

--- bpf/xdp/xdp_dump.c
+++ bpf/xdp/xdp_dump.c
@@ -1,3 +1,17 @@
+#include "common.h"
 
+SEC("xdp")
+int xdp_dump(struct xdp_md *ctx)
+{
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    __u64 packet_size = data_end - data;
 
-
+    // L2
+    struct ethhdr *ether = data;
+    if (data + sizeof(*ether) > data_end)
+    {
+        return XDP_ABORTED;
+    }
+    return 0;
+}
